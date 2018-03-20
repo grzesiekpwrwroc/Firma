2b39cee7e38085ecabfdedff1b3eb77bedcb7ce2
Grzesiek <grzegorz.jarus@gmail.com>
2018-03-20 01:54:48 +0100

    Dodanie możliwości dania podwyżki pracownikom

--- src/WyswietlFrame.java
+++ src/WyswietlFrame.java
@@ -15,36 +15,55 @@ public class WyswietlFrame extends JFrame {
             JTable table = new JTable(new ModelData(DodajFrame.getLista()));
             //table.setBounds(10,10,100,200);
             add(new JScrollPane(table),BorderLayout.PAGE_START);
+            JTextField tekstFieldProcent = new JTextField("Procent podwyżki");
+            JTextField tekstFieldBudzet = new JTextField("Budżet");
 
-            //JButton przyciskFiltruj=new JButton("Filtruj");
-            //add(przyciskFiltruj,BorderLayout.WEST);
-            //JTable table2 = new JTable(new ModelData((Filtry.filtrujPensjaPowyzejProgu(10000)).filtrujRokUrodzeniaPowyzejProgu(1980)));
-           //JTable table2 = new JTable(new ModelData(Filtry.filtrujRokUrodzaniaPowyzejProgu(true,Filtry.filtrujPensjaPowyzejProgu(true,DodajFrame.getLista(),9900),1980)));
-           // table2.setBounds(10,600,200,200);
-            //add(new JScrollPane(table));
-            //add(new JScrollPane(table2));
-            //add(table);
-            //add(table2);
-            JButton usun = new JButton("Usuń");
-            usun.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    ModelData model = (ModelData) table.getModel();
-                    Pracownik usuniety = null;
-                    try {
-                        int wiersz = table.getSelectedRow();
-                        usuniety = model.data.get(wiersz);
-                        model.removeRow(wiersz);
-                    } catch (Exception ex) {
-                        JOptionPane.showMessageDialog(null, ex);
-                    }
-                    table.updateUI();
-                    try{
-                        JOptionPane.showMessageDialog(null, "Usunięto pracownika"+usuniety.wyswietlPracownika());
-                    } catch(NullPointerException e1){
-                        JOptionPane.showMessageDialog(null, e1.getMessage());
-                    }
+            JButton dajPodwyzke = new JButton("Daj podwyżkę");
+
+            add(new JScrollPane(tekstFieldProcent),BorderLayout.WEST);
+            add(new JScrollPane(tekstFieldBudzet),BorderLayout.CENTER);
+            add(new JScrollPane(dajPodwyzke),BorderLayout.EAST);
+
+            dajPodwyzke.addActionListener(e -> {
+                ModelData model = (ModelData) table.getModel();
+                Pracownik wybrany = null;
+                try{
+                    int wiersz = table.getSelectedRow();
+                    wybrany = model.data.get(wiersz);
+
+                    DodajFrame.setLista(Pracownik.czyPodwyzkaMozliwa(wybrany,Float.parseFloat(tekstFieldProcent.getText()),Long.parseLong(tekstFieldBudzet.getText())));
+                    //new ModelData(Pracownik.czyPodwyzkaMozliwa(wybrany,Float.parseFloat(tekstFieldProcent.getText()),Long.parseLong(tekstFieldBudzet.getText())));
+                   // model.setData(DodajFrame.getLista());//=new ModelData(DodajFrame.getLista());
+                    model = new ModelData(DodajFrame.getLista());
+                    table.setModel(model);
+                }catch(Exception e1){
+                    DodajFrame.setLista(Pracownik.czyPodwyzkaMozliwa(null,Float.parseFloat(tekstFieldProcent.getText()),Long.parseLong(tekstFieldBudzet.getText())));
+                    //JOptionPane.showMessageDialog(null,"Błąd");
+                    model = new ModelData(DodajFrame.getLista());
+                    table.setModel(model);
+                }table.updateUI();
+            });
+
+
+            JButton usun = new JButton("Usuń pracownika");
+            usun.addActionListener(e -> {
+                ModelData model = (ModelData) table.getModel();
+                Pracownik usuniety = null;
+                try {
+                    int wiersz = table.getSelectedRow();
+                    usuniety = model.data.get(wiersz);
+                    model.removeRow(wiersz);
+                    //usuniety.wyswietlPracownikaOkrojona();
+                    JOptionPane.showMessageDialog(null, "Usunięto pracownika:" + usuniety.wyswietlPracownika());
                 }
+                catch(NullPointerException e1){
+                        JOptionPane.showMessageDialog(null, e1.getMessage());
+                        }
+                catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, " Zaznacz pracownika do usunięcia");
+                    }
+                table.updateUI();
+
             });
             add(new JScrollPane(usun),BorderLayout.PAGE_END);
             setVisible(true);
